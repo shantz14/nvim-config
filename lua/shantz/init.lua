c5222ed5017a7b51b4dcdf2352295108ba359db8
Isaac Schwarz <ids45@nau.edu>
2024-10-13 17:14:04 -0700

    Initial commit.

---
+++ lua/shantz/init.lua
@@ -0,0 +1,10 @@
+require("shantz.remap")
+require("shantz.set")
+
+vim.api.nvim_set_option("clipboard", "unnamed")
+
+-- prime recs
+vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'n', 'nzzzv', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'N', 'Nzzzv', { noremap = true, silent = true })
---
+++ lua/shantz/packer.lua
@@ -0,0 +1,66 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use ({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
+    })
+
+    use ({
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    })
+
+    use {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = function()
+            require("nvim-autopairs").setup {}
+        end
+    }
+
+    use {
+        "windwp/nvim-ts-autotag",
+        event = "InsertEnter",
+        config = function()
+            require("nvim-ts-autotag").setup {}
+        end
+    }
+
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('theprimeagen/harpoon')
+    use('mbbill/undotree')
+
+    use ('neovim/nvim-lspconfig')
+    use ('williamboman/mason.nvim')
+    use ('williamboman/mason-lspconfig.nvim')
+    use ('hrsh7th/nvim-cmp')
+    use ('hrsh7th/cmp-nvim-lsp')
+    use ('hrsh7th/cmp-buffer')
+    use ('hrsh7th/cmp-path')
+    use ('saadparwaiz1/cmp_luasnip')
+    use ('L3MON4D3/LuaSnip')
+    use ('rafamadriz/friendly-snippets')
+
+end)
+-- Gaming gametime
+-- I love to game it is so cool
+-- Um hello um hello lets get this fricking bread gamers hehehehehehhehehe
+-- OK so now its time to set up a color scheme cuz this aint cutting it pal
+-- the real question is whether or not to have the line in insert mode or not
+-- it is pretty pog so idk gamer lets get this bread tho am i right
