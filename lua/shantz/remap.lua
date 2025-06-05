7a1ada8cdf7134b4ddb0c2ceb80ec050319b1937
shantz14 <isaac@steveandkureen.com>
2025-06-05 01:07:59 -0700

    oil fix

--- lua/shantz/remap.lua
+++ lua/shantz/remap.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader =  " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", function()
+  require("oil").open()
+end, { desc = "Open parent directory in Oil" })
 
 vim.keymap.set("n", "<leader>/", vim.cmd.nohlsearch)
 
